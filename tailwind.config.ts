98cd9cb2914abab441ffa2cfa066f04c6caf78dc
Nicholas Vuong <nicholas.k.vuong@gmail.com>
2025-11-11 13:11:00 -0600

    added more styling to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,6 +66,44 @@ export default {
           ring: "hsl(var(--sidebar-ring))",
         },
       },
-    }
-  }
-}
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+        "scroll-up": {
+          "0%": {
+            transform: "translateY(0)",
+          },
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "scroll-up": "scroll-up linear infinite",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
